ce3031a7674872dd9f2a07f2511de257131220a4
Roche Kollie <35652808+rochekollie@users.noreply.github.com>
2022-11-27 00:41:27 -0500

    Reduce project structure.

--- projects/mixed-messages/script.mjs
+++ projects/mixed-messages/script.mjs
@@ -1,112 +1,43 @@
 import {getContrastYIQ, getRandomColor} from './beautify.mjs';
-import {createCard} from './modules/createCard.mjs';
-import {queryResource} from './modules/resource.mjs';
+import {companies, databases, frameworks, languages, platforms, tools} from './data.mjs';
+
 
 document.addEventListener('DOMContentLoaded', () => {
+
     const randomElement = (array) => {
         return array[Math.floor(Math.random() * array.length)];
     };
 
-    //const quotes = await queryResource('technology is fun');
-
-    const generateMessage = (nouns, verbs, adjectives) => {
-        const noun = randomElement(nouns);
-        const verb = randomElement(verbs);
-        const adjective = randomElement(adjectives);
-        const mantra = `${noun} ${verb} ${adjective}`;
-        return mantra.charAt(0).toUpperCase() + mantra.slice(1);
+    const getMessage = () => {
+        const noun = randomElement(languages);
+        const verb = randomElement(frameworks);
+        const adjective = randomElement(databases);
+        const adverb = randomElement(tools);
+        const preposition = randomElement(platforms);
+        const company = randomElement(companies);
+        const message = `${noun} ${verb} ${adjective}`;
+        return message.charAt(0).toUpperCase() + message.slice(1);
     };
 
-    function getMantras(components) {
-        components.forEach(function (component) {
-            component.innerHTML = generateMessage(nouns, verbs, adjectives);
-        });
-    }
-
-    function colorize(components) {
-        components.forEach(function (section) {
-            section.style.backgroundColor = getRandomColor();
-            section.style.color = getContrastYIQ(getRandomColor());
-        });
-    }
-
-    const createCardComponents = (count) => {
-        const components = [];
-        for (let i = 0; i < count; i += 1) {
-            components.push(createCard());
+    function displayMessage() {
+        const messageContainer = document.getElementById('message');
+        if (messageContainer) {
+            messageContainer.innerHTML = getMessage();
         }
-        return components;
-    };
-
-    function displayQuotes(promise) {
-        const totalQuotes = 20; // change this to change the number of quotes
-        const quoteComponents = createCardComponents(totalQuotes);
-        const quoteContainer = document.getElementById('quote-container');
-
-        const quoteCards = document.getElementById('quote-wrapper');
-        if (quoteCards) {
-            quoteCards.innerHTML = '';
-        }
-
-        // get the user query
-        const query = document.getElementById('search-field').value;
-
-        // get the quotes from the API
-        const quotes = queryResource(query);
-
-        // create the quote cards
-        quotes.then((data) => {
-            data.results.forEach((quote, index) => {
-                const quoteCard = quoteComponents[index];
-                const quoteParagraph = quoteCard.querySelector('.quote');
-                const quoteAuthor = quoteCard.querySelector('.quote-author');
-                const quoteTag = quoteCard.querySelector('.tag');
-                quoteParagraph.innerHTML = quote.content;
-                quoteAuthor.innerHTML = quote.author;
-                quoteTag.innerHTML = quote.tags;
-                quoteCards.appendChild(quoteCard);
-            });
-        });
-
-        // display the quote cards
-        quoteContainer.style.display = 'block';
     }
 
-    const getQuoteTag = () => {
-        const tags = document.querySelectorAll('.tags');
-        let quoteTag = '';
-        tags.forEach((tag) => {
-            tag.addEventListener('click', (event) => {
-                quoteTag = event.target.innerHTML;
-            });
-        });
-        return quoteTag;
-    };
-
-    const autoFillField = (input, value) => {
-        if (input && value) {
-            input.value = value;
-        }
-    };
-
-    const searchField = document.getElementById('search-field');
-    autoFillField(searchField, getQuoteTag());
-
-    function searchQuotesByTagName(tagValue) {
-        queryResource(tagValue).then((data) => {
-            console.log(data);
-        });
+    function decorateCard(card) {
+        card.style.backgroundColor = getRandomColor();
+        card.style.color = getContrastYIQ(getRandomColor());
     }
 
-    const getQuoteButton = document.querySelector('#search-button');
+    // Add event listener to the button
+    document.querySelector('button')?.addEventListener('click', displayMessage);
 
-    // Attach an event listener to the `button`
-    getQuoteButton?.addEventListener('click', displayQuotes);
+    // load the page with a message
+    displayMessage();
+    decorateCard(document.querySelector('card')); // experiment and delete it
 
-    // load the page with quotes
-    //displayQuotes();
-    //colorize(document.querySelectorAll('section')); // experiment and delete it
-
-    // call the searchQuotesByTagName function
-    searchQuotesByTagName();
+    // call the displayMessage function
+    getMessage();
 });
